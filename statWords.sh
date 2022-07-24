14d0a11e1dc684762a686914432ca92a95a626cd
Pablo <pablopistelli88@gmail.com>
2022-07-24 19:39:48 -0300

    Se modifica script para leer palabras desde archivo.

--- statWords.sh
+++ statWords.sh
@@ -1,45 +1,46 @@
 #!/bin/bash
 
-##########################################
+#############################################################################################
 #
 # Indicador estadistico de longitud de palabra (mas larga, mas corta y promedio de longitud)
 #
-##########################################
-ARGUMENTOS=$@
-SOLO_PALABRAS="^[A-Za-z]+$"
+#############################################################################################
 
-[ $# -eq 0 ] && echo "Debe ingresar al menos un argumento!" && exit 1
+FILE=$1
 
+SOLO_PALABRAS="^[A-Za-z]+$"	#Regex para omitir construcciones de caracteres que no sean palabras.
 
-MAYOR=""
-MENOR=$1
-CONT=0
-SUMA=0
-for palabra in $ARGUMENTOS
-do
-	if [[ $palabra =~ $SOLO_PALABRAS ]] 
-	then
-		if [ ${#palabra} -gt ${#MAYOR} ]
-		then
+MAYOR=0		#Palabra de mayor longitud
+MENOR=0		#Palabra de menor longitud
+CONT=0		#Contador de palabras
+SUMA=0		#Suma total de caracteres del archivo
+
+while read linea; do
+    for palabra in $linea; do
+	#Analizo sólamente las palabras
+        if [[ $palabra =~ $SOLO_PALABRAS ]]; then	
+		#Si es la primera palabra leída la guardo como más larga y más corta
+		if [ $CONT == 0 ]; then
 			MAYOR=$palabra
-		fi
-		
-		if [ ${#palabra} -le ${#MENOR} ]
-		then	
 			MENOR=$palabra
+		else
+		#Si no es la primera palabra la comparo con las palabras guardadas
+			[ ${#palabra} -gt ${#MAYOR} ] && MAYOR=$palabra
+			[ ${#palabra} -lt ${#MENOR} ] && MENOR=$palabra
 		fi
-	else
-		echo "Debe ingresar solo palabras!" && exit 2
+
+		((CONT++))			#Aumento contador
+		SUMA=$((SUMA+${#palabra}))	#Acumulo cantidad de caracteres
 	fi
-	
-	SUMA=$((SUMA+${#palabra}))
-	((CONT++))
-done
+    done
+done <$FILE
 
-PROMEDIO=$((SUMA/CONT))
+#Calculo promedio de cantidad de caracteres por palabra
+PROMEDIO=$((SUMA/CONT))		
 
-echo "Palabra mas larga: $MAYOR"
-echo "Palabra mas corta: $MENOR"
+echo "Palabra mas larga: $MAYOR - ${#MAYOR} letras"
+#Determino si la palabra mas corta tiene una letra o más para mostrar el mensaje correcto (letra/letras)
+([ ${#MENOR} -eq 1 ] && echo "Palabra mas corta: $MENOR - ${#MENOR} letra") || echo "Palabra mas corta: $MENOR - ${#MENOR} letras" 
 echo "Promedio de longitud de palabras: $PROMEDIO letras"
 
 exit 0
