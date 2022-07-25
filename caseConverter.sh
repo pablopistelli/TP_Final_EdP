2f810b46c498c6b74ac641e447264158b2616702
Pablo <pablopistelli88@gmail.com>
2022-07-25 20:22:07 -0300

    Se ordena salida por consola. Probado.

--- caseConverter.sh
+++ caseConverter.sh
@@ -8,8 +8,14 @@ FILE=$1
 FILE_CONT=$(cat $FILE)		#Guardo el contenido del archivo
 FILE_INV="${FILE_CONT~~}"	#Invierto el case de los caracteres (forma no documentada, verificar funcionamiento en Container)
 
-echo "Texto original: $FILE_CONT"
-echo "Texto invertido: $FILE_INV"
+echo "Texto original:"
+echo "---------------"
+echo "$FILE_CONT"
+echo " "
+
+echo "Texto invertido:"
+echo "----------------"
+echo "$FILE_INV"
 
 exit 0
 
