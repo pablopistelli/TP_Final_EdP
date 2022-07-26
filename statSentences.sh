65d606119ce0b9fd7469d078dc9089c25b164aaf
Pablo <pablopistelli88@gmail.com>
2022-07-25 22:38:34 -0300

    Se corrige deteccion de espacios en blanco para no contabilizarlos como caracteres.

--- statSentences.sh
+++ statSentences.sh
@@ -12,12 +12,14 @@ CANT_OR=0	#Cantidad de oraciones
 LONG=0		#Longitud de oracion actual
 
 FIN_OR="[.!?]"	#Regex para detectar fin de oraciones 
-OMIT_OR="\s"	#Regex para omitir caracteres -----> NO PUEDO IDENTIFICAR ESPACIOS EN BLANCO
 
 while read -n1 char; do
 	#Cuento cantidad de caracteres hasta encontrar un final de oracion
 	if [[ ! $char =~ $FIN_OR ]]; then
-		LONG=$(($LONG+1)) && echo "$LONG $char"  #Cuento la cantidad de caracteres
+		#Cuento la cantidad de caracteres omitiendo los espacios en blanco
+		if [[ $char = [![:space:]] ]]; then
+			LONG=$(($LONG+1))
+		fi
 	else
 		#Si es la primera oracion que leo la guardo como la más corta y la más larga
 		if [ $CANT_OR == 0 ]; then
