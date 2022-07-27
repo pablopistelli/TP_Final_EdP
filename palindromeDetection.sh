3fbb3e2bfd1a49a15ab07b2fbeec6ffcfbf3e15f
Eugenio Lopez <96elopez@gmail.com>
2022-07-26 21:43:04 -0300

    Script detector de palindromos

---
+++ palindromeDetection.sh
@@ -0,0 +1,24 @@
+FILE=$1		
+
+#-------------------------
+# DETECCION DE PALINDROMO
+#-------------------------
+
+
+while read linea; do
+	for palabra in $linea; do
+		
+		#Convierto el código de caracteres de cada palabra leída para eliminar acentos
+		SIN_ACENTOS=$(echo $palabra | iconv -f UTF-8 -t ASCII//TRANSLIT)
+		
+		#Revierto el orden de de los caracteres
+	        REVERSE=$(echo $SIN_ACENTOS | rev)
+	        
+	        #Matcheo ambas variables para encontrar el palindromo
+	        if [[ ${SIN_ACENTOS,,} =~ ${REVERSE,,} ]]; then
+                	echo "$palabra es un palindromo"
+                fi
+	done
+done <$FILE
+
+exit 0
