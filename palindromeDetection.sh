7fc600834463b34f82b88e4a6d2c885b2e7b03a1
Eugenio Lopez <96elopez@gmail.com>
2022-07-27 19:33:36 -0300

    Arreglo errores

--- palindromeDetection.sh
+++ palindromeDetection.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-		
+
 #-------------------------
 # DETECCION DE PALINDROMO
 #-------------------------
@@ -9,18 +9,22 @@ FILE=$1
 while read linea; do
 	for palabra in $linea; do
 		
-		#Convierto el código de caracteres de cada palabra leída para eliminar acentos
-		SIN_ACENTOS=$(echo $palabra | iconv -f UTF-8 -t ASCII//TRANSLIT)
+		if [ ${#palabra} -gt 2 ]; then 
+			#Elimino caracteres que me crean problemas
+			ELIM_CAR=$(echo $palabra | tr -d '.,?!')
 		
-		#Revierto el orden de de los caracteres
-	        REVERSE=$(echo $SIN_ACENTOS | rev)
+			#Convierto el código de caracteres de cada palabra leída para eliminar acentos
+			SIN_ACENTOS=$(echo $ELIM_CAR | iconv -f UTF-8 -t ASCII//TRANSLIT)
+		
+			#Revierto el orden de los caracteres
+	      	 	REVERSE=$(echo $SIN_ACENTOS | rev)
 	        
-	        #Matcheo ambas variables para encontrar el palindromo
-	        if [[ ${SIN_ACENTOS,,} =~ ${REVERSE,,} ]]; then
-                	echo "$palabra es un palindromo"
+	      	  	#Convierto a minusculas y matcheo ambas variables para encontrar el palindromo
+	        	if [[ ${SIN_ACENTOS,,} =~ ${REVERSE,,} ]]; then
+                		echo "$ELIM_CAR es un palindromo"
+                	fi
                 fi
 	done
 done <$FILE
 
 exit 0
-
