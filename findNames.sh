8ad376e8986b276799881adfa7740ad32a6477c6
eugenio <91112771+euglpz@users.noreply.github.com>
2022-08-01 13:18:32 -0300

    Modifico script. Probado.
    Modifico script para que tome nombres propios seguidos de caracteres ",.?!".

--- findNames.sh
+++ findNames.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-####################################
-#
-# Identificacion de nombres propios
-#
-####################################
+#----------------------------------
+# Identificación de nombres propios
+#----------------------------------
 
 FILE=$1		
 NOMBRE_PROPIO="^[A-Z]{1}[a-z]+$"	#Regex para formato de nombre propio: Una mayúscula + Al menos una minúscula
 
+#Elimino caracteres indeseados e incorporo salto de linea.
+ARREGLO=$(cat $FILE | tr -d '.,?!' | tr ' ' '\n')
+
 echo "Nombres propios encontrados en el archivo:"
 
-while read linea; do
-	for palabra in $linea; do
-	        #Comparo las palabras leidas con la regex para nombres propios y muestro en pantalla las coincidencias
-		if [[ $palabra =~ $NOMBRE_PROPIO ]]; then
-                	echo $palabra
-        	fi
-	done
-done <$FILE
-
-exit 0
+for palabra in $ARREGLO
+do
+	#Comparo las palabras leidas con la regex para nombres propios y muestro en pantalla las coincidencias
+	if [[ $palabra =~ $NOMBRE_PROPIO ]]
+	then
+		echo $palabra
+	fi
+done
