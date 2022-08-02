9bdb657c4d1e7d6b286b87c2c9bd75e4c6f4ac8d
eugenio <91112771+euglpz@users.noreply.github.com>
2022-08-02 17:29:57 -0300

    Update findNames.sh
    Modifico para que tome nombre propio finalizado con ":"

--- findNames.sh
+++ findNames.sh
@@ -8,7 +8,7 @@ FILE=$1
 NOMBRE_PROPIO="^[A-Z]{1}[a-z]+$"	#Regex para formato de nombre propio: Una mayúscula + Al menos una minúscula
 
 #Elimino caracteres indeseados e incorporo salto de linea.
-ARREGLO=$(cat $FILE | tr -d '.,?!' | tr ' ' '\n')
+ARREGLO=$(cat $FILE | tr -d '.,?!:' | tr ' ' '\n')
 
 echo "Nombres propios encontrados en el archivo:"
 
