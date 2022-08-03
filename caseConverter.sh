2d32c2c69e81c60dba69ab4c4581ab60e4d57cd9
eugenio <91112771+euglpz@users.noreply.github.com>
2022-08-03 17:59:58 -0300

    Update caseConverter.sh

--- caseConverter.sh
+++ caseConverter.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#--------------------------
-# MAYUSCULAS <-> minusculas
-#--------------------------
+#--------------------------------------------------------------------
+# Invertir minúsculas a mayúsculas y viceversa, de todas las palabras.
+#--------------------------------------------------------------------
 
 FILE=$1
 FILE_CONT=$(cat $FILE)		#Guardo el contenido del archivo
-FILE_INV="${FILE_CONT~~}"	#Invierto el case de los caracteres (forma no documentada, verificar funcionamiento en Container)
+FILE_INV="${FILE_CONT~~}"	#Invierto el case de los caracteres
 
 echo "Texto original:"
 echo "---------------"
